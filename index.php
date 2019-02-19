964245558fdc365aab2533b7db10ef59bb274f4e
Eragon32312 <marcmonsterhunter4@gmail.com>
2019-02-19 09:44:52 +0100

    OO

--- index.php
+++ index.php
@@ -1,58 +1,29 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
+	<head>
+		    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <title>Fallout76</title>
-  </head>
-  <body style="color: #000000; background-image:url(images/cascosfondo.jpg)">
+    <title>Array</title>
+	</head>
+	<body>
 
-      <nav aria-label="breadcrumb">
-    <ol style="background-color: #000000" class="breadcrumb">
-      <li style="background-color: #000000" class="breadcrumb-item active" aria-current="page">
-        <font color="#ffffff"> <h1>WIKI FALLOUT FANDOM</h1>
-                 <div class="container">
-        <div class="row">
-          <div class="col-sm">
-            the future is now
-          </div>
-          <div class="col-sm">
- 
-          </div>
-          <div class="row">
-          <div class="col-sm">
-            <img src="images/black_shop.jpg" alt="black space" style="width:400px;height:100px;">
-          </div>
-          <div class="col-sm">
-            <div class="container-fluid">
-            <img src="images/fallout.png" alt="logo Fallout76" style="width:280px;height:130px;">
-          </div>
-        </div>
-      </div>
-          </li>
-        </ol>
-      </nav>
-        <div class="container">
+	<?php
 
-        </div>
-         <div class="col-sm">
-            <div class="container-fluid">
-            <img src="images/fallout-76-creature.jpg" alt="logo Fallout76" style="width:480px;height:330px;">
-          </div>
-        <h2>mostro grafton</h2>
-        </div>
-        </div>
-         <div class="col-sm">
-            <div class="container-fluid">
-            <img src="images/fallout-76-screen-3.jpg" alt="logo Fallout76" style="width:480px;height:330px;">
-          </div>
-        <h2>bestia ardente</h2>
-        </div>
+	$arr = ["mela","kiwi","pera"];
+
+	echo $arr[1];
+		
+	?>
+
+
+
+
+	
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
